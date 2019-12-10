98161a338806e961cee482d8a83257511745228e
Sebastian Moreno Castañeda <smorenoc@car.gov.co>
2019-12-10 17:38:41 -0500

    creacion de formulario y validacion de funciones con codigo protegido y creacion de formulario

---
+++ formulario.php
@@ -0,0 +1,66 @@
+<center>
+    <h1>formulario de control de registros</h1>
+    
+    <div class="col-12">
+     	<div class="col-4" style="float:left;">
+     		<div class="col-12">
+         		<div class="label">
+         			<label>CC</label>
+         		</div>
+         		<div class="input">
+         			<input TYPE="number" ID="CC" NAME="CC" VALUE=""/>
+         		</div>
+     		</div>
+     		
+     		<div class="col-12">
+         		<div class="label">NOMBRE</div>
+         		<div class="input">
+         			<input type="text" id="nom" NAME="nom," VALUE=""/>
+         		</div>
+     		
+     		</div>
+     		
+     		<div>APELLIDO</div>
+     		<div>
+     			<input type="text" id="ape" NAME="ape" VALUE=""/>
+     		</div>
+     		<div>CORREO</div>
+     		<div>
+     			<input type="text" id="correo" NAME="correo" VALUE=""/>
+     		</div>
+     		<div>TELEFONO</div>
+     		<div>
+     			<input type="number" id="tel" NAME="tel" VALUE=""/>
+     		</div>
+     		<div>CIUDAD</div>
+     		<div>
+     			<select id="ciudad" name="ciudad">
+     				<option value="" selected>seleccione
+     				<option value="1">BOGOTA
+     				<option value="2">CALI
+     				<option value="3">MEDELLIN
+     			</select>
+     		</div>
+     		<div>GENERO</div>
+     		<div>
+     			<label>Femenino</label> 
+     			<input TYPE="RADIO" ID="GENERO" NAME="GENERO"/>
+     			<label>Masculino</label> 
+     			<input TYPE="RADIO" ID="GENERO" NAME="GENERO"/>
+     			<label>LGTBI</label> 
+     			<input TYPE="RADIO" ID="GENERO" NAME="GENERO"/>
+     		</div>
+     		<div>
+     			<button>Registrar</button>
+     			<button>Cancelar</button>
+     		</div>
+     		
+     		
+     	</div>
+     	
+     	<div class="col-6" style="float:right;">
+     	
+     	</div>
+     	
+    </div>
+</center>
--- js/Mi_Logica.js
+++ js/Mi_Logica.js
@@ -1,71 +1,107 @@
 
+$(document).ready(function(){
+	
 
-/*
- * url: es donde voy a ir
- * parametros: que voy a llevar
- * metodo: que quiero hacer con la respuesta
- */
-function fajax(URL,parametros,metodo){
-	$.ajax({
-		url: URL,
-		data: parametros,
-		type:'POST',
-		cache: false,
-		dataType: 'html',
-		success: function (datos){
-			metodo(datos);
-		},
-		error: function (xhr,status){
-			alert("EXISTE UN PROBLEMA")
-		}
+	/*
+	 * url: es donde voy a ir
+	 * parametros: que voy a llevar
+	 * metodo: que quiero hacer con la respuesta
+	 * 
+	 */
+	function fajax(URL,parametros,metodo){
+		$.ajax({
+			url: URL,
+			data: parametros,
+			type:'POST',
+			cache: false,
+			dataType: 'html',
+			success: function (datos){
+				metodo(datos);
+			},
+			error: function (xhr,status){
+				alert("EXISTE UN PROBLEMA")
+			}
+		});
+		
+	}
+
+	/**
+	 * 
+	 * traer vista del planeta mercurio
+	 */
+
+	function traermercurio (){
+		var url = "mercurio.php";
+		var parametros = "acceso=true";
+		var metodo=function (datos){
+			$("#contenido").html(datos);
+		};
+		
+		fajax(url,parametros,metodo);
+	}
+
+	/**
+	 * 
+	 * traer vista del planeta venus
+	 */
+
+	function traervenus (){
+		var url = "venus.php";
+		var parametros = "acceso=true";
+		var metodo=function (datos){
+			$ ("#contenido").html(datos);
+		};
+		
+		fajax(url,parametros,metodo);
+	}
+
+	/**
+	 * 
+	 * traer vista del planeta tierra
+	 */
+
+	function traertierra (){
+		var url = "tierra.php";
+		var parametros = "acceso=true";
+		var metodo=function (datos){
+			$ ("#contenido").html(datos);
+		};
+		
+		fajax(url,parametros,metodo);
+	}
+	
+	/**
+	 * 
+	 * formulario de ingreso de datos
+	 */
+
+	function formulario (){
+		var url = "formulario.php";
+		var parametros = "acceso=true";
+		var metodo=function (datos){
+			$ ("#contenido").html(datos);
+		};
+		
+		fajax(url,parametros,metodo);
+	}
+	
+	$("#traermercurio").click(function(){
+		traermercurio();
 	});
 	
-}
-
-/**
- * 
- * traer vista del planeta mercurio
- */
-
-function traermercurio (){
-	var url = "mercurio.php";
-	var parametros = "acceso=true";
-	var metodo=function (datos){
-		$("#contenido").html(datos);
-	};
+	$("#traervenus").click(function(){
+		traervenus();
+	});
 	
-	fajax(url,parametros,metodo);
-}
-
-/**
- * 
- * traer vista del planeta venus
- */
-
-function traervenus (){
-	var url = "venus.php";
-	var parametros = "acceso=true";
-	var metodo=function (datos){
-		$ ("#contenido").html(datos);
-	};
+	$("#traertierra").click(function(){
+		traertierra();
+	});
 	
-	fajax(url,parametros,metodo);
-}
+	$("#formulario").click(function(){
+		formulario();
+	});
+});
 
-/**
- * 
- * traer vista del planeta tierra
- */
-
-function traertierra (){
-	var url = "tierra.php";
-	var parametros = "acceso=true";
-	var metodo=function (datos){
-		$ ("#contenido").html(datos);
-	};
-	
-	fajax(url,parametros,metodo);
-}
 
 
 
